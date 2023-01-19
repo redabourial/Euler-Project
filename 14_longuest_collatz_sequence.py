1f878aab7bffcccd724727549e55b9658a2c728b
Reda Bourial <reda.bourial@gmail.com>
2023-01-19 22:06:38 +0100

    implemented longuest collatz

---
+++ 14_longuest_collatz_sequence.py
@@ -0,0 +1,18 @@
+def get_chain_length(n):
+    chain_length = 0
+    while n != 1:
+        chain_length += 1
+        if n % 2 == 0:
+            n = n/2
+        else:
+            n = 3*n+1
+    return chain_length
+
+longuest_chain,number = 0,1
+for n in range(2,1000*1000+1):
+    chain_length = get_chain_length(n)
+    if chain_length> longuest_chain:
+        longuest_chain = chain_length
+        number=n
+
+print(number)
