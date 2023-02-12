a5683c5e050605c6a50008f25aea3a5fd610899c
Reda Bourial <reda.bourial@gmail.com>
2023-02-12 04:11:43 +0100

    optimization

--- 14_longuest_collatz_sequence.py
+++ 14_longuest_collatz_sequence.py
@@ -2,10 +2,10 @@ def get_chain_length(n):
     chain_length = 0
     while n != 1:
         chain_length += 1
-        if n % 2 == 0:
-            n = n/2
-        else:
+        if n % 2:
             n = 3*n+1
+        else:
+            n = n/2
     return chain_length
 
 longuest_chain,number = 0,1
--- 16_power_digit_sum.py
+++ 16_power_digit_sum.py
@@ -1,3 +1,3 @@
 number = 2**1000
-sum_of_digits = sum([int(c) for c in str(number)])
+sum_of_digits = sum((int(c) for c in str(number)))
 print(sum_of_digits)
